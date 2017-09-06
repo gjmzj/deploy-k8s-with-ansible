3646999f84b19267364feb7f5f0cafbb3bb5ca89
jmgao <jmgaozz@163.com>
2017-09-06 17:42:01 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,24 @@
+# 利用Ansible部署kubernetes集群
+
+本文档记录自己实践部署高可用k8s集群的过程，利用ansible-playbook简化二进制方式部署过程。
+
+网上有很多类似shell脚本和ansible部署版本，要不看得太复杂，或者久未更新，所以这里自己造轮子吧。
+
+二进制方式手动部署，将有助于理解系统各组件的交互原理和熟悉组建启动参数，进而能快速解决实际问题。
+
+1. 建议阅读 [feisky.gitbooks](https://feisky.gitbooks.io/kubernetes/) 原理和部署章节。
+1. 建议阅读 [opsnull教程](https://github.com/opsnull/follow-me-install-kubernetes-cluster) 二进制手工部署。
+
+本文是按照上述文档，更新组件实践修饰而成。
+
+## 特性
+
+1. 截至2017-8-24 最新组件版本，参见[down版本](./down/download.sh) 文件。
+1. 因本人部署节点IP属于同一网段，使用flannel新后端[host-gw](https://github.com/coreos/flannel/blob/master/Documentation/backends.md) 提升部分性能。
+
+## 步骤
+
+1. 准备4台虚机(物理机也可，虚机实验更方便)，安装Ubuntu16.04(centos7理论上一样，不想ansible脚本太多条件判断)
+1. 准备一台部署机(可以复用上述4台虚机)，安装ansible，配置到4台目标机器ssh无密码登陆等
+1. 准备外部负载均衡，准备master节点的vip地址
+1. 规划集群节点，完成ansible inventory文件[参考](hosts) 
---
+++ down/download.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#主要组件版本如下
+export K8S_VER=v1.7.4
+export ETCD_VER=v3.2.6
+export FLANNEL_VER=v0.8.0
+export DOCKER_VER=17.06.1-ce
+
+echo "\n----download k8s binary at:"
+echo https://dl.k8s.io/${K8S_VER}/kubernetes-server-linux-amd64.tar.gz
+
+echo "\n----download etcd binary at:"
+echo https://github.com/coreos/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz
+echo https://storage.googleapis.com/etcd/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+echo "\n----download flannel binary at:"
+echo https://github.com/coreos/flannel/releases/download/${FLANNEL_VER}/flannel-${FLANNEL_VER}-linux-amd64.tar.gz
+
+echo "\n----download docker binary at:"
+echo https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VER}.tgz
+
+echo "\n----download ca tools at:"
+echo https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+echo https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+echo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
+
+echo "\n因为网络原因不进行自动下载，否则脚本执行不可控制"
+echo "请按照以上链接手动下载二进制包到down目录中，包含如下："
+echo "-rw-r--r-- 1 root root   6595195 Mar 30  2016 cfssl-certinfo_linux-amd64"
+echo "-rw-r--r-- 1 root root   2277873 Mar 30  2016 cfssljson_linux-amd64"
+echo "-rw-r--r-- 1 root root  10376657 Mar 30  2016 cfssl_linux-amd64"
+echo "-rwxr--r-- 1 root root  29699281 Aug 24 17:09 docker-17.06.1-ce.tgz*"
+echo "-rwxr--r-- 1 root root  10176124 Aug 24 17:09 etcd-v3.2.6-linux-amd64.tar.gz*"
+echo "-rwxr--r-- 1 root root   9090192 Aug 24 17:09 flannel-v0.8.0-linux-amd64.tar.gz*"
+echo "-rwxr--r-- 1 root root 437406487 Aug 24 17:09 kubernetes-server-linux-amd64.tar.gz*"
+
+mkdir -p ../bin
+mv cfssl_linux-amd64 ../bin/cfssl
+mv cfssljson_linux-amd64 ../bin/cfssljson
+mv cfssl-certinfo_linux-amd64 ../bin/cfssl-certinfo
+
+echo "\nextracting etcd binaries..."
+tar zxf etcd-${ETCD_VER}-linux-amd64.tar.gz
+mv etcd-${ETCD_VER}-linux-amd64/etcd* ../bin
+
+echo "\nextracting flannel binaries..."
+tar zxf flannel-${FLANNEL_VER}-linux-amd64.tar.gz
+mv flanneld mk-docker-opts.sh ../bin
+rm README.md
+
+echo "\nextracting kubernetes binaries..."
+tar zxf kubernetes-server-linux-amd64.tar.gz
+mv kubernetes/server/bin/kube-apiserver ../bin
+mv kubernetes/server/bin/kube-controller-manager ../bin
+mv kubernetes/server/bin/kubectl ../bin
+mv kubernetes/server/bin/kubelet ../bin
+mv kubernetes/server/bin/kube-proxy ../bin
+mv kubernetes/server/bin/kube-scheduler ../bin
+
+echo "\nextracting docker binaries..."
+tar zxf docker-${DOCKER_VER}.tgz
+mv docker/docker* ../bin
